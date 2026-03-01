6c969549d12948b2b44a38c9d447f14efb503ad1
michaelprys <musha09cz@gmail.com>
2026-03-01 16:48:28 +0100

    feat: add on-change reactivity for appState

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ class App {
         },
     ];
 
+    appState = {
+        favorites: [],
+    };
+
     constructor() {
         window.addEventListener('hashchange', this.handleRoute.bind(this));
 
@@ -22,7 +26,7 @@ class App {
 
         const view = this.routes.find((r) => r.path === location.hash).view;
 
-        this.currentView = new view();
+        this.currentView = new view(this.appState);
         this.currentView.render();
     }
 }
